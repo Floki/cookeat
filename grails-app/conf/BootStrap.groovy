332c0c21a98dad31513bb2c363215f3a71c05c74
Simar Jeremy <exasky@live.fr>
2014-01-09 21:53:15 +0100

    Add users and recipe to bootstrap for tests (with permissions)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,17 +1,26 @@
 import org.junit.internal.runners.statements.FailOnTimeout;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
 
 import cookeat.recipe.Recipe;
+import cookeat.user.Role
 import cookeat.user.User
+import cookeat.user.UserRole
 
 class BootStrap {
 
     def init = { servletContext ->
-		User user1 = new User(username:"user1",password:"test",email:"user1@test.test")
+		Role adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true, FailOnError : true)
+		
+		User user1 = new User(username:"user1",password:"test",email:"user1@test.test",)
 		user1.save(failOnError : true)
 		User user2 = new User(username:"user2",password:"test",email:"user2@test.test")
 		user2.save(FailOnError : true)
 		User user3 = new User(username:"user3",password:"test",email:"user3@test.test")
 		user3.save(FailOnError : true)
+		
+		UserRole.create user1, adminRole, true;
+		UserRole.create user2, adminRole, true;
+		UserRole.create user3, adminRole, true;
 		
 		Map<String,String> ingr = new HashMap();
 		ingr.put("Poulet", "1kg");
