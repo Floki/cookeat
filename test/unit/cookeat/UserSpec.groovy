77ceb75563c265cbd6a6d075ec722a69f210b13a
Robin <bussenot.robin@gmail.com>
2014-01-18 19:14:01 +0100

    Unit test for vote [#9]

--- test/unit/cookeat/UserSpec.groovy
+++ test/unit/cookeat/UserSpec.groovy
@@ -9,7 +9,7 @@ import spock.lang.Specification
  */
 @TestFor(User)
 class UserSpec extends Specification {
-
+						
     def setup() {
     }
 
--- test/unit/cookeat/recipe/VoteSpec.groovy
+++ test/unit/cookeat/recipe/VoteSpec.groovy
@@ -1,5 +1,6 @@
 package cookeat.recipe
 
+import cookeat.user.User;
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Vote)
+@Mock([Recipe,User])
 class VoteSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,35 @@ class VoteSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-    }
+    void "create vote"() {
+    
+	when: "create valid test"
+	
+	then:
+	new Vote(owner: own,recipe:rec,rate:note).validate();
+	
+	where:
+	own		  | rec           |note
+	new User()| new Recipe()| 0
+	new User()| new Recipe()|5
+	new User()| new Recipe()|4
+	
+	
+	}
+	
+	void "create non valid vote"(){
+		
+		when: "create valid test"
+		
+		then:
+		!(new Vote(owner: own,recipe:rec,rate:note).validate());
+		
+		where:
+		own		  | rec           |note
+		new User()| new Recipe()| -1
+		new User()| new Recipe()|6
+		new User()|null|4
+		null| new Recipe()|4
+		
+	}
 }
