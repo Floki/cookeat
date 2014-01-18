e49fcbcac3b73469f1e2b91ecc52473355d50725
Robin <bussenot.robin@gmail.com>
2014-01-18 19:38:42 +0100

    Unit test for Comment [#10]

--- test/unit/cookeat/recipe/CommentSpec.groovy
+++ test/unit/cookeat/recipe/CommentSpec.groovy
@@ -1,5 +1,6 @@
 package cookeat.recipe
 
+import cookeat.user.User;
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Comment)
+@Mock([User,Recipe])
 class CommentSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,32 @@ class CommentSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-    }
+    void "create non valid comment"() {
+
+		when: "non valid comment"
+		
+		then:
+		!(new Comment( owner:own, recipe :rec, text: txt).validate());
+		
+		where:
+		own		  | rec           |txt
+		new User()| new Recipe()| null
+		null| new Recipe()|null
+		new User()|null|""
+		null| new Recipe()|"text"
+		
+	}
+	
+	void "create valid comment"() {
+	
+	when: "valid comment"
+		
+		then:
+		(new Comment( owner:own, recipe :rec, text: txt).validate());
+		
+		where:
+		own		  | rec           |txt
+		new User()| new Recipe()| "new comment"
+	
+	}
 }
