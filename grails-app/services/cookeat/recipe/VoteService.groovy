73cea3617bdced16f23109db3a59aa0cf40f9300
Simar Jeremy <exasky@live.fr>
2014-01-23 12:01:26 +0100

    #16 Add CRUD on vote :)

--- grails-app/services/cookeat/recipe/VoteService.groovy
+++ grails-app/services/cookeat/recipe/VoteService.groovy
@@ -1,26 +1,33 @@
 package cookeat.recipe
 
+import cookeat.user.User
 import grails.transaction.Transactional
 
 @Transactional
 class VoteService {
 
-    def createVote() {
-
+    def createVote(User owner, Recipe recipe, int rate) {
+		Vote vote = new Vote(owner: owner, recipe: recipe, rate: rate)
+		return vote.save()
     }
 	
-	def readVote(){
-	
+	def readAllVoteOnRecipe(Recipe recipe){
+		List<Vote> listVote =  Vote.getAll()
+		List<Vote> result = new ArrayList<Vote>()
+		for (var in listVote) {
+			if (var.getRecipe() == recipe)
+				result.add(var)
+		}
+		return result
 	}
 	
 	def updateVote(Vote actual, int newRate){
-		def vote = Vote.get(actual)
-		vote.rate = newRate
-		vote.save()
+		actual.rate = newRate
+		return actual.save()
 	}
 	
-	def deleteVote(){
-		
+	def deleteVote(Vote vote){
+		vote.delete()
 	}
 	
 	
--- test/integration/cookeat/recipe/VoteServiceIntegrationSpec.groovy
+++ test/integration/cookeat/recipe/VoteServiceIntegrationSpec.groovy
@@ -10,8 +10,7 @@ import cookeat.user.User;
  */
 class VoteServiceIntegrationSpec extends Specification {
 	
-	Vote v1
-	VoteService voteService = new VoteService()
+	VoteService voteService 
 
     def setup() {
 		
@@ -22,12 +21,67 @@ class VoteServiceIntegrationSpec extends Specification {
 
     void "test vote update"() {
 		setup:
-		def user = Mock(User)
-		def recipe = Mock(Recipe)
-		v1 = new Vote(owner: user, recipe: recipe, rate: 3)
-		
-	expect:
-		voteService.updateVote(v1,0)
-		voteService.updateVote(v1,5)
+			def user = new User(email: "test@test.test", username: "test", password: "test" )
+			user.save(failOnError : true)
+			def recipe = new Recipe(ingredients: new HashMap<String, String>(), title: "title", owner: user, recipe: "test" )
+			recipe.save(failOnError: true)
+			def v1 = new Vote(owner: user, recipe: recipe, rate: 3)
+			v1.save(failOnError: true)
+			
+		expect:
+			voteService.updateVote(v1,2) == v1
+			voteService.updateVote(v1,12) == null
     }
+	
+	void "test create vote"(){
+		setup:
+			def user = new User(email: "test@test.test", username: "test", password: "test" )
+			user.save(failOnError : true)
+			def recipe = new Recipe(ingredients: new HashMap<String, String>(), title: "title", owner: user, recipe: "test" )
+			recipe.save(failOnError: true)
+			
+		expect:
+			voteService.createVote(user, recipe, 0)
+			voteService.createVote(user, recipe, 10) == null
+	}
+	
+	void "test delete vote"(){
+		setup: 
+			def user = new User(email: "test@test.test", username: "test", password: "test" )
+			user.save(failOnError : true)
+			def recipe = new Recipe(ingredients: new HashMap<String, String>(), title: "title", owner: user, recipe: "test" )
+			recipe.save(failOnError: true)
+			def v1 = new Vote(owner: user, recipe: recipe, rate: 3)
+			v1.save(failOnError: true)
+			
+		when:
+			voteService.deleteVote(v1)
+			
+		then:
+			Vote.getAll().empty == true
+	}
+	
+	void "test read all votes on one recipe"(){
+		setup:	
+			def user = new User(email: "test@test.test", username: "test", password: "test" )
+			user.save(failOnError : true)
+			
+			def recipe1 = new Recipe(ingredients: new HashMap<String, String>(), title: "recipe1", owner: user, recipe: "recipe1" )
+			def recipe2 = new Recipe(ingredients: new HashMap<String, String>(), title: "recipe2", owner: user, recipe: "recipe2" )
+			recipe1.save(failOnError: true)
+			recipe2.save(failOnError: true)
+			
+			def v1 = new Vote(owner: user, recipe: recipe1, rate: 3)
+			def v2 = new Vote(owner: user, recipe: recipe2, rate: 3)
+			v1.save(failOnError: true)
+			v2.save(failOnError: true)
+			
+		when:
+			List<Vote> actual = voteService.readAllVoteOnRecipe(recipe1)
+			
+		then:	
+			actual.contains(v1) == true
+			actual.contains(v2) == false
+			
+	}
 }
